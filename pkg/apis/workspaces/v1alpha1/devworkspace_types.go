548195fba1f9bd759aa905048f03d93acedef3da
David Festal <dfestal@redhat.com>
2020-01-16 18:36:08 +0100

    First sketch of refactored commands
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/apis/workspaces/v1alpha1/devworkspace_types.go
+++ pkg/apis/workspaces/v1alpha1/devworkspace_types.go
@@ -19,7 +19,7 @@ type DevWorkspaceSpec struct {
 type DevWorkspaceTemplateSpec struct {
 	// Predefined, ready-to-use, workspace-related commands
 	Commands          []Command      `json:"commands,omitempty"`
-	
+
 	// Projects worked on in the workspace, containing names and sources locations
 	Projects          []Project      `json:"projects,omitempty"`
 	
