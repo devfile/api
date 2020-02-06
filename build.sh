10166425ecf6e76d5bb72eb1d860ec27b6e6a7d3
David Festal <dfestal@redhat.com>
2020-02-06 18:30:48 +0100

    Fix `shellcheck` errors
    Signed-off-by: David Festal <dfestal@redhat.com>

--- build.sh
+++ build.sh
@@ -42,34 +42,35 @@ then
   exit 1
 fi
 
-command -v operator-sdk >/dev/null 2>&1 || { echo -e $RED"operator-sdk is not installed. Aborting."$NC; exit 1; }
+command -v operator-sdk >/dev/null 2>&1 || { echo -e "${RED}operator-sdk is not installed. Aborting.${NC}"; exit 1; }
 
 operatorVersion=$(operator-sdk version)
-[[ $operatorVersion =~ .*v0.12.0.* ]] || { echo -e $RED"operator-sdk v0.12.0 is required"$NC; exit 1; }
+[[ $operatorVersion =~ .*v0.12.0.* ]] || { echo -e "${RED}operator-sdk v0.12.0 is required${NC}"; exit 1; }
 
 set -e
 
 BASE_DIR=$(cd "$(dirname "$0")" && pwd)
 
-mkdir -p ${BASE_DIR}/generated
+mkdir -p "${BASE_DIR}/generated"
 
 operator-sdk generate k8s
 operator-sdk generate openapi
 yq '.spec.validation.openAPIV3Schema' \
-  ${BASE_DIR}/deploy/crds/workspaces.ecd.eclipse.org_devworkspaces_crd.yaml \
-  > ${BASE_DIR}/schemas/devworkspace.json
+  "${BASE_DIR}/deploy/crds/workspaces.ecd.eclipse.org_devworkspaces_crd.yaml" \
+  > "${BASE_DIR}/schemas/devworkspace.json"
 
-jq ".properties.spec.properties.template" ${BASE_DIR}/schemas/devworkspace.json > ${BASE_DIR}/schemas/devworkspace-template-spec.json
+jq ".properties.spec.properties.template" "${BASE_DIR}/schemas/devworkspace.json" > "${BASE_DIR}/schemas/devworkspace-template-spec.json"
 
-cp ${BASE_DIR}/schemas/devworkspace-template-spec.json ${BASE_DIR}/schemas/devfile.json
+cp "${BASE_DIR}/schemas/devworkspace-template-spec.json" "${BASE_DIR}/schemas/devfile.json"
 
 onError() {
   echo "Cleaning schemas/devfile.json"
-  rm -f ${BASE_DIR}/schemas/devfile.json
+  rm -f "${BASE_DIR}/schemas/devfile.json"
 }
 trap 'onError' ERR
 
-for jsonpatch in $(ls ${BASE_DIR}/devfile-support/transformation-rules/*.json 2> /dev/null)
+for jsonpatch in "${BASE_DIR}"/devfile-support/transformation-rules/*.json
 do
-  jsonpatch -i --indent 2 ${BASE_DIR}/schemas/devfile.json ${jsonpatch}
+  [ -e "$jsonpatch" ] || continue
+  jsonpatch -i --indent 2 "${BASE_DIR}/schemas/devfile.json" "${jsonpatch}"
 done
