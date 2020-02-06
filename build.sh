19b703436ca0859eafb1bbdcb4b4d066bd203d57
David Festal <dfestal@redhat.com>
2020-02-06 17:55:02 +0100

    Add a copyright
    Signed-off-by: David Festal <dfestal@redhat.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# Copyright (c) 2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
 
 BLUE='\033[1;34m'
 GREEN='\033[0;32m'
