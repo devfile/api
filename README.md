33b2ab5bfc3b0761aa9dc6f170f5441deb695543
David Festal <dfestal@redhat.com>
2020-02-06 21:37:03 +0100

    Reorganize repository to clarify relationship betwen the devworkspace API and devfile 2.0 (#23)
    * Restructure the repository in order to:
      - Make a clearer separation between devworkspace and devfile schemas,
      - Provide jsonpatch rules to derive the devfile schema from the devworkspace one
      - Isolate devfile-related elements in a dedicated folder (jsonpatch rules and devfile samples)
      - Generate all the schemas from the Go code in a single build command
      - Allow running the `build` command in a docker container that contains the prerequisites
    * Add a `devfile.yaml`
    * Add theia settings to setup json schema bindings
    * Links in readmes
    * Add basic intro + how to build in the readme
    
    Signed-off-by: David Festal <dfestal@redhat.com>
    
    Co-authored-by: Angel Misevski <amisevsk@redhat.com>
    Co-authored-by: Florent BENOIT <fbenoit@redhat.com>
    Co-authored-by: Mario Loriedo <mario.loriedo@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# devworkspace-api
-K8S Api for a cloud develoment workspace specification [Draft proposal]
+# Kube-native API for cloud development workspaces specification
+
+Sources for this API are defined in Go code, starting from the
+[devworkspace_types.go source file](pkg/apis/workspaces/v1alpha1/devworkspace_types.go)
+
+From these Go sources, several files are generated:
+- A Kubernetes Custom Resource Definition with an embedded OpenApi schema,
+- json schemas (in the [schemas](schemas) folder) generated from the above Custom Resource Definition, to specify the syntax of:
+  - the DevWorkspace CRD itself
+  - the DevWorkspaceTemplate CRD (a workspace content, without runtime information),
+  - the Devfile 2.0.0 format, which is generated from the `DevWorkspace` API.
+
+Generated files are created by a build script (see section [How to build](#how-to-build)).
+
+### Devfile 2.0.0 file format
+
+A Subset of this `DevWorkspace` API defines a structure (workspace template content), which is also at the core of the **Devfile 2.0** format specification.
+For more information about this, please look into
+the [Devfile support Readme](devfile-support/README.md)
+
+## How to build
+
+In order to build the CRD and the various schemas, you don't need to install any pre-requisite apart from `docker`.
+In the root directory, just run the following command:
+
+```
+./docker-run.sh build.sh
+``` 
+## Specification status
+
+This work is still in an early stage of specification, and the related API and schemas are still a draft proposal. 
---
+++ build.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# Copyright (c) 2020 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+BLUE='\033[1;34m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+BOLD='\033[1m'
+
+if ! command -v yq &> /dev/null
+then
+  echo
+  echo "#### ERROR ####"
+  echo "####"
+  echo "#### Please install the 'yq' tool before being able to use this script"
+  echo "#### see https://github.com/kislyuk/yq"
+  echo "#### and https://stedolan.github.io/jq/download"
+  echo "####"
+  echo "###############"
+  exit 1
+fi
+
+if ! command -v jsonpatch &> /dev/null
+then
+  echo
+  echo "#### ERROR ####"
+  echo "####"
+  echo "#### Please install the 'jsonpatch' tool before being able to use this script"
+  echo "#### For this use:"
+  echo "####    pip3 install --user jsonpatch"
+  echo "####"
+  echo "###############"
+  exit 1
+fi
+
+command -v operator-sdk >/dev/null 2>&1 || { echo -e "${RED}operator-sdk is not installed. Aborting.${NC}"; exit 1; }
+
+operatorVersion=$(operator-sdk version)
+[[ $operatorVersion =~ .*v0.12.0.* ]] || { echo -e "${RED}operator-sdk v0.12.0 is required${NC}"; exit 1; }
+
+set -e
+
+BASE_DIR=$(cd "$(dirname "$0")" && pwd)
+
+mkdir -p "${BASE_DIR}/generated"
+
+operator-sdk generate k8s
+operator-sdk generate openapi
+yq '.spec.validation.openAPIV3Schema' \
+  "${BASE_DIR}/deploy/crds/workspaces.ecd.eclipse.org_devworkspaces_crd.yaml" \
+  > "${BASE_DIR}/schemas/devworkspace.json"
+
+jq ".properties.spec.properties.template" "${BASE_DIR}/schemas/devworkspace.json" > "${BASE_DIR}/schemas/devworkspace-template-spec.json"
+
+cp "${BASE_DIR}/schemas/devworkspace-template-spec.json" "${BASE_DIR}/schemas/devfile.json"
+
+onError() {
+  echo "Cleaning schemas/devfile.json"
+  rm -f "${BASE_DIR}/schemas/devfile.json"
+}
+trap 'onError' ERR
+
+for jsonpatch in "${BASE_DIR}"/devfile-support/transformation-rules/*.json
+do
+  [ -e "$jsonpatch" ] || continue
+  jsonpatch -i --indent 2 "${BASE_DIR}/schemas/devfile.json" "${jsonpatch}"
+done
