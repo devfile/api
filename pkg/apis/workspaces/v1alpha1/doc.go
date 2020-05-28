bf00af5e43897f87dfb37c47311bb9933ef89644
David Festal <dfestal@redhat.com>
2020-05-28 18:56:37 +0200

    switch the API group to `devfile.io`
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/apis/workspaces/v1alpha1/doc.go
+++ pkg/apis/workspaces/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the org v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=workspaces.ecd.eclipse.org
+// +groupName=devfile.io
 package v1alpha1
--- pkg/apis/workspaces/v1alpha1/register.go
+++ pkg/apis/workspaces/v1alpha1/register.go
@@ -2,7 +2,7 @@
 
 // Package v1alpha1 contains API Schema definitions for the org v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=workspaces.ecd.eclipse.org
+// +groupName=devfile.io
 package v1alpha1
 
 import (
@@ -12,7 +12,7 @@ import (
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "workspaces.ecd.eclipse.org", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "devfile.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
