02d756116e7c9479ce2bdf711bea9ae024beb0ed
David Festal <dfestal@redhat.com>
2020-06-22 19:20:49 +0200

    Finalize endpoints (#84)
    * Implement endpoint changes in a distinct file
    * Add endpoints in kubernetes and openshift components
    * Add `https` and `wss` protocols
    * Regenerate CRDs and schemas
    * Update samples
    
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/apis/workspaces/v1alpha1/containerComponent.go
+++ pkg/apis/workspaces/v1alpha1/containerComponent.go
@@ -8,44 +8,6 @@ type ContainerComponent struct {
 	Endpoints     []Endpoint `json:"endpoints,omitempty"`
 }
 
-type Endpoint struct {
-	Name string `json:"name"`
-
-	// +optional
-	TargetPort int `json:"targetPort,omitempty"`
-
-	// +optional
-	Configuration *EndpointConfiguration `json:"configuration,omitempty"`
-
-	// +optional
-	Attributes map[string]string `json:"attributes,omitempty"`
-}
-
-type EndpointConfiguration struct {
-	// +optional
-	Public bool `json:"public"`
-	// +optional
-	Discoverable bool `json:"discoverable"`
-	// The is the low-level protocol of traffic coming through this endpoint.
-	// Default value is "tcp"
-	// +optional
-	Protocol string `json:"protocol,omitmepty"`
-	// The is the URL scheme to use when accessing the endpoint.
-	// Default value is "http"
-	// +optional
-	Scheme string `json:"scheme,omitmepty"`
-	// +optional
-	Secure bool `json:"secure"`
-	// +optional
-	CookiesAuthEnabled bool `json:"cookiesAuthEnabled"`
-	// +optional
-	Path string `json:"path,omitempty"`
-
-	// +kubebuilder:validation:Enum=ide;terminal;ide-dev
-	// +optional
-	Type string `json:"type,omitempty"`
-}
-
 type Container struct {
 	Name string `json:"name"`
 
@@ -63,11 +25,13 @@ type Container struct {
 	MemoryLimit string `json:"memoryLimit,omitempty"`
 
 	// The command to run in the dockerimage component instead of the default one provided in the image.
+	//
 	// Defaults to an empty array, meaning use whatever is defined in the image.
 	//+optional
 	Command []string `json:"command,omitempty"`
 
 	// The arguments to supply to the command running the dockerimage component. The arguments are supplied either to the default command provided in the image or to the overridden command.
+	//
 	// Defaults to an empty array, meaning use whatever is defined in the image.
 	//+optional
 	Args []string `json:"args,omitempty"`
