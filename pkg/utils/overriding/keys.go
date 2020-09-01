f5bcbc623c4b0631f46e1bacea35ac477cadf610
David Festal <dfestal@redhat.com>
2020-09-01 16:33:44 +0200

    Add comments in the function
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/utils/overriding/keys.go
+++ pkg/utils/overriding/keys.go
@@ -8,31 +8,43 @@ import (
 
 type checkFn func(elementType string, keysSets []sets.String) []error
 
+// checkKeys provides a generic way to apply some validation on the content of each type of top-level list
+// contained in the `toplevelListContainers` passed in argument.
+//
+// For each type of top-level list, the `keysSets` argument that will be passed to the `doCheck` function
+// contains the the key sets that correspond to the `toplevelListContainers` passwed to this method,
+// in the same order.
 func checkKeys(doCheck checkFn, toplevelListContainers ...workspaces.TopLevelListContainer) error {
 	var errors *multierror.Error
 
-	listOfTopLevelLists := []workspaces.TopLevelLists{}
+	// Retrieve the top-level lists (Commands, Projects, Components) for each TopLevelListContainer
+	// (DevWorkspaceTemplateSpec, PluginOverrides, ...)
+	topLevelListsByContainer := []workspaces.TopLevelLists{}
 	for _, toplevelListContainer := range toplevelListContainers {
-		listOfTopLevelLists = append(listOfTopLevelLists, toplevelListContainer.GetToplevelLists())
+		topLevelListsByContainer = append(topLevelListsByContainer, toplevelListContainer.GetToplevelLists())
 	}
 
-	elementTypes := sets.String{}
-	for _, toplevelLists := range listOfTopLevelLists {
-		for elementType := range toplevelLists {
-			elementTypes.Insert(elementType)
+	// Gather all types of top-level lists: Commands, Projects, etc ...
+	listTypes := sets.String{}
+	for _, toplevelLists := range topLevelListsByContainer {
+		for listType := range toplevelLists {
+			listTypes.Insert(listType)
 		}
 	}
 
-	for _, elementType := range elementTypes.List() {
+	// For each type of top-level list (Commands, Projects), etc ...
+	for _, listType := range listTypes.List() {
+	    // For each toplevel-list container, extract the set of keys of the given type of top-level list 
 		keySets := []sets.String{}
-		for _, toplevelLists := range listOfTopLevelLists {
+		for _, toplevelLists := range topLevelListsByContainer {
 			keys := sets.String{}
-			for _, keyed := range toplevelLists[elementType] {
+			for _, keyed := range toplevelLists[listType] {
 				keys.Insert(keyed.Key())
 			}
 			keySets = append(keySets, keys)
 		}
-		checkErrors := doCheck(elementType, keySets)
+		// Perform the check on the sets of keys for the given type of top-level list
+		checkErrors := doCheck(listType, keySets)
 		errors = multierror.Append(errors, checkErrors...)
 	}
 
