3465390b28039c68fb26c1db19b1429bc55dc2c1
David Festal <dfestal@redhat.com>
2020-09-25 11:53:26 +0200

    Fix review comment from @amisevsk...
    https://github.com/devfile/api/pull/133#discussion_r494574966
    https://github.com/devfile/api/pull/133#discussion_r494577454
    https://github.com/devfile/api/pull/133#discussion_r494578627
    https://github.com/devfile/api/pull/133#discussion_r494582669
    Signed-off-by: David Festal <dfestal@redhat.com>

--- generator/genutils/utils.go
+++ generator/genutils/utils.go
@@ -72,44 +72,50 @@ func AddUnionOneOfConstraints(jsonSchema *apiext.JSONSchemaProps, unionDiscrimin
 			return
 		}
 
-		if len(schema.Properties) > 0 {
-		discriminatorLoop:
-			for _, discriminator := range unionDiscriminators {
-				discriminatorPropertyName := strcase.ToLowerCamel(discriminator.Name)
-				if discriminatorProp, found := schema.Properties[discriminatorPropertyName]; found &&
-					discriminatorProp.Enum != nil &&
-					len(discriminatorProp.Enum) != 0 {
-					oneOf := []apiext.JSONSchemaProps{}
-					newEnumValues := []apiext.JSON{}
-				enumValueLoop:
-					for _, enumValue := range discriminatorProp.Enum {
-						fieldName := strings.Trim(string(enumValue.Raw), `"`)
-						unionMemberProperty := strcase.ToLowerCamel(fieldName)
-						if _, foundUnionMember := schema.Properties[unionMemberProperty]; !foundUnionMember {
-							continue discriminatorLoop
-						}
-						for _, toSkip := range fieldsToSkip {
-							if toSkip == fieldName {
-								continue enumValueLoop
-							}
-						}
-						newEnumValues = append(newEnumValues, enumValue)
-						oneOf = append(oneOf, apiext.JSONSchemaProps{
-							Required: []string{unionMemberProperty},
-						})
-					}
-					schema.OneOf = oneOf
-					discriminatorProp.Enum = newEnumValues
+		if len(schema.Properties) == 0 {
+			return
+		}
 
-					if removeDiscriminators {
-						delete(schema.Properties, discriminatorPropertyName)
-					} else {
-						schema.Properties[discriminatorPropertyName] = discriminatorProp
-					}
-					for _, toSkip := range fieldsToSkip {
-						delete(schema.Properties, strcase.ToLowerCamel(toSkip))
+	discriminatorLoop:
+		for _, discriminator := range unionDiscriminators {
+			discriminatorPropertyName := strcase.ToLowerCamel(discriminator.Name)
+
+			discriminatorProp, found := schema.Properties[discriminatorPropertyName]
+			if !found || len(discriminatorProp.Enum) == 0 {
+				continue discriminatorLoop
+			}
+
+			oneOf := []apiext.JSONSchemaProps{}
+			newEnumValues := []apiext.JSON{}
+		enumValueLoop:
+			for _, enumValue := range discriminatorProp.Enum {
+				fieldName := strings.Trim(string(enumValue.Raw), `"`)
+				unionMemberProperty := strcase.ToLowerCamel(fieldName)
+				if _, foundUnionMember := schema.Properties[unionMemberProperty]; !foundUnionMember {
+					// The discriminator enum contains values that do not correspond to any enum field.
+					// So so just skip the oneOf creation here since this union definition seems invalid.
+					continue discriminatorLoop
+				}
+				for _, toSkip := range fieldsToSkip {
+					if toSkip == fieldName {
+						continue enumValueLoop
 					}
 				}
+				newEnumValues = append(newEnumValues, enumValue)
+				oneOf = append(oneOf, apiext.JSONSchemaProps{
+					Required: []string{unionMemberProperty},
+				})
+			}
+			schema.OneOf = oneOf
+			discriminatorProp.Enum = newEnumValues
+
+			if removeDiscriminators {
+				delete(schema.Properties, discriminatorPropertyName)
+			} else {
+				schema.Properties[discriminatorPropertyName] = discriminatorProp
+			}
+			for _, toSkip := range fieldsToSkip {
+				delete(schema.Properties, strcase.ToLowerCamel(toSkip))
 			}
 		}
 		return
