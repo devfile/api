8dffab5c063e88625677d2731eb472e1f36aa011
David Festal <dfestal@redhat.com>
2020-09-28 19:10:37 +0200

    Add a generator to validate the API GO source code
    Signed-off-by: David Festal <dfestal@redhat.com>

--- generator/validate/gen.go
+++ generator/validate/gen.go
@@ -1,14 +1,15 @@
-package interfaces
+package validate
 
 import (
 	"fmt"
 	"go/ast"
+	"go/types"
 	"strings"
 
 	"github.com/devfile/api/generator/genutils"
-	"github.com/iancoleman/strcase"
 	crdmarkers "sigs.k8s.io/controller-tools/pkg/crd/markers"
 	"sigs.k8s.io/controller-tools/pkg/genall"
+	"sigs.k8s.io/controller-tools/pkg/loader"
 	"sigs.k8s.io/controller-tools/pkg/markers"
 )
 
@@ -51,17 +52,17 @@ func (g Generator) Generate(ctx *genall.GenerationContext) error {
 
 		for _, typeToCheck := range packageTypes {
 			if typeToCheck.Markers.Get(genutils.UnionMarker.Name) != nil {
-				// Check Union
+				// Check Unions
 				unionMembers := []string{}
 				var unionDiscriminatorField *markers.FieldInfo
 				for index, unionField := range typeToCheck.Fields {
 					if unionField.Markers.Get(genutils.UnionDiscriminatorMarker.Name) != nil {
 						if unionDiscriminatorField != nil {
-							root.AddError(fmt.Errorf(
-								"Union %v should have only 1 union discriminator, but has 2: %v and %v",
+							root.AddError(loader.ErrFromNode(fmt.Errorf(
+								"Union `%v` should have only 1 union discriminator, but has 2: `%v` and `%v`",
 								typeToCheck.Name,
 								unionDiscriminatorField.Name,
-								unionField.Name))
+								unionField.Name), unionField.RawField))
 						}
 						unionDiscriminatorField = &(typeToCheck.Fields[index])
 					} else {
@@ -69,25 +70,99 @@ func (g Generator) Generate(ctx *genall.GenerationContext) error {
 					}
 				}
 				if unionDiscriminatorField == nil {
-					root.AddError(fmt.Errorf(
-						"Union %v should have 1 union discriminator. See here for details: https://github.com/kubernetes/enhancements/blob/master/keps/sig-api-machinery/20190325-unions.md#proposal",
-						typeToCheck.Name))
-				}
+					root.AddError(loader.ErrFromNode(fmt.Errorf(
+						"Union `%v` should have 1 union discriminator. See here for details: https://github.com/kubernetes/enhancements/blob/master/keps/sig-api-machinery/20190325-unions.md#proposal",
+						typeToCheck.Name), typeToCheck.RawSpec))
+				} else {
+					if unionDiscriminatorField.Markers.Get("optional") == nil {
+						root.AddError(loader.ErrFromNode(fmt.Errorf(
+							"In union `%v` the union discriminator `%v` should have the `+optional` comment marker",
+							typeToCheck.Name,
+							unionDiscriminatorField.Name), unionDiscriminatorField.RawField))
+					}
+					if !strings.Contains(unionDiscriminatorField.Tag.Get("json"), ",omitempty") {
+						root.AddError(loader.ErrFromNode(fmt.Errorf(
+							"In union `%v` the union discriminator `%v` should contain the `omitempty` option in its `json` tag, since it is expected to be an optional field",
+							typeToCheck.Name,
+							unionDiscriminatorField.Name), unionDiscriminatorField.RawField))
+					}
 
-				if unionDiscriminatorField.Markers.Get("optional") == nil {
-					root.AddError(fmt.Errorf(
-						"In union %v the union discriminator %v should have the `+optional` comment marker",
+					wrongTypeError := loader.ErrFromNode(fmt.Errorf(
+						"In union `%v` the union discriminator `%v` should have a `string` type, or a type (defined in the same package) whose underlying type is a string",
 						typeToCheck.Name,
-						unionDiscriminatorField.Name))
+						unionDiscriminatorField.Name), unionDiscriminatorField.RawField)
+
+					discriminatorTypeRef, typeFound := root.TypesInfo.Types[unionDiscriminatorField.RawField.Type]
+					if !typeFound {
+						root.AddError(wrongTypeError)
+					}
+
+					underlyingType, underlyingTypeIsBasic := discriminatorTypeRef.Type.Underlying().(*types.Basic)
+					if !underlyingTypeIsBasic || underlyingType.Kind() != types.String {
+						root.AddError(wrongTypeError)
+					}
+
+					getEnumValidation := func(markers markers.MarkerValues, node ast.Node) *crdmarkers.Enum {
+						enumMarkerIf := markers.Get("kubebuilder:validation:Enum")
+						if enumMarkerIf != nil {
+							if enumMarker, isEnum := enumMarkerIf.(crdmarkers.Enum); isEnum {
+								return &enumMarker
+							}
+							root.AddError(loader.ErrFromNode(fmt.Errorf(
+								"In union `%v` the union discriminator `%v` should only have the kubebuilder marker that defines the allowed union values as an Enum (kubebuilder:validation:Enum=....)",
+								typeToCheck.Name,
+								unionDiscriminatorField.Name), node))
+						}
+						return nil
+					}
+
+					enumMarkerPtr := getEnumValidation(unionDiscriminatorField.Markers, unionDiscriminatorField.RawField)
+					var enumAstNode ast.Node = unionDiscriminatorField.RawField
+
+					if _, isBasic := discriminatorTypeRef.Type.(*types.Basic); !isBasic {
+						if typeNamed, isNamed := discriminatorTypeRef.Type.(*types.Named); isNamed {
+							enumTypeInfo, isInSamePackage := packageTypes[typeNamed.Obj().Name()]
+							if !isInSamePackage {
+								root.AddError(loader.ErrFromNode(fmt.Errorf(
+									"In union `%v` the union discriminator `%v` should have a string-based type defined in the same package",
+									typeToCheck.Name,
+									unionDiscriminatorField.Name), unionDiscriminatorField.RawField))
+							} else {
+								typeEnumMarkerPtr := getEnumValidation(enumTypeInfo.Markers, enumTypeInfo.RawSpec)
+								if typeEnumMarkerPtr != nil {
+									if enumMarkerPtr != nil {
+										root.AddError(loader.ErrFromNode(fmt.Errorf(
+											"Type `%v` should not define a `kubebuilder:validation:Enum` annotation, since it is already defined in the discriminator `%v` of union `%v`",
+											enumTypeInfo.Name,
+											unionDiscriminatorField.Name,
+											typeToCheck.Name), enumTypeInfo.RawSpec))
+									} else {
+										enumMarkerPtr = typeEnumMarkerPtr
+										enumAstNode = enumTypeInfo.RawSpec
+									}
+								}
+							}
+						}
+					}
+
+					if enumMarkerPtr == nil {
+						root.AddError(loader.ErrFromNode(fmt.Errorf(
+							"In union `%v` the union discriminator `%v` should specify the allowed union values through an annotation (kubebuilder:validation:Enum=....)",
+							typeToCheck.Name,
+							unionDiscriminatorField.Name), unionDiscriminatorField.RawField))
+					} else {
+						for index, enumValue := range *enumMarkerPtr {
+							stringValue, isString := enumValue.(string)
+							if !isString || stringValue != unionMembers[index] {
+								root.AddError(loader.ErrFromNode(fmt.Errorf(
+									"In union `%v` the union discriminator `%v` should allow the following values through the `kubebuilder:validation:Enum` anntation: "+strings.Join(unionMembers, ";"),
+									typeToCheck.Name,
+									unionDiscriminatorField.Name), enumAstNode))
+								break
+							}
+						}
+					}
 				}
-				if unionDiscriminatorField.Tag.Get("json") == "" {
-					root.AddError(fmt.Errorf(
-						"In union %v the union discriminator %v should have the `+optional` comment marker",
-						typeToCheck.Name,
-						unionDiscriminatorField.Name))
-				}
-				// Simplify unions writing by adding the kubebuiler enum annotation ourselves in the CRD + Json schema generation.
-				// => Nothing more to check here, well, yes, still check thatno enum is added manually at least.
 			}
 		}
 
