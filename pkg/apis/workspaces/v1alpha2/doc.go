c6c36c2dd5f5a0394ee4a11b661cfc2ea50aa961
Angel Misevski <amisevsk@redhat.com>
2020-09-28 20:21:06 -0400

    Add license headers to existing files
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/apis/workspaces/v1alpha2/doc.go
+++ pkg/apis/workspaces/v1alpha2/doc.go
@@ -1,3 +1,15 @@
+//
+// Copyright (c) 2019-2020 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
 // Package v1alpha2 contains API Schema definitions for the org v1alpha2 API group
 // +k8s:deepcopy-gen=package,register
 // +k8s:openapi-gen=true
--- pkg/apis/workspaces/v1alpha2/register.go
+++ pkg/apis/workspaces/v1alpha2/register.go
@@ -1,3 +1,15 @@
+//
+// Copyright (c) 2019-2020 Red Hat, Inc.
+// This program and the accompanying materials are made
+// available under the terms of the Eclipse Public License 2.0
+// which is available at https://www.eclipse.org/legal/epl-2.0/
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// Contributors:
+//   Red Hat, Inc. - initial API and implementation
+//
+
 // NOTE: Boilerplate only.  Ignore this file.
 
 // Package v1alpha2 contains API Schema definitions for the org v1alpha2 API group
