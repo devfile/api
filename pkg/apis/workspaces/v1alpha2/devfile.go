a272a21f5a00acd1e75b43c355123c697f80f0c0
David Festal <dfestal@redhat.com>
2020-10-09 18:29:44 +0200

    Extract the `DevfileHeader` from the K8S API.
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/apis/workspaces/v1alpha2/devfile.go
+++ pkg/apis/workspaces/v1alpha2/devfile.go
@@ -1,25 +1,13 @@
 package v1alpha2
 
+import (
+	"github.com/devfile/api/pkg/devfile"
+)
+
 // Devfile describes the structure of a cloud-native workspace and development environment.
 // +devfile:jsonschema:generate:omitCustomUnionMembers=true
 type Devfile struct {
-	// Devfile schema version
-	// +kubebuilder:validation:Pattern=^2\.([0-9]+)\.([0-9]+)(\-[0-9a-z-]+(\.[0-9a-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$
-	SchemaVersion string `json:"schemaVersion"`
-
-	// +kubebuilder:pruning:PreserveUnknownFields
-	// +optional
-	// Optional metadata
-	Metadata DevfileMetadata `json:"metadata,omitempty"`
+	devfile.DevfileHeader `json:",inline"`
 
 	DevWorkspaceTemplateSpec `json:",inline"`
-}
-
-type DevfileMetadata struct {
-	// Optional devfile name
-	Name string `json:"name,omitempty"`
-
-	// Optional semver-compatible version
-	// +kubebuilder:validation:Pattern=^([0-9]+)\.([0-9]+)\.([0-9]+)(\-[0-9a-z-]+(\.[0-9a-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$
-	Version string `json:"version,omitempty"`
 }
