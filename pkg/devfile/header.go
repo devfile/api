22c70a164200c33a0b8cfc47f4d2bdf70cef20a5
David Festal <dfestal@redhat.com>
2020-11-10 14:11:09 +0100

    Add a way to get the free-form metadata fields
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/devfile/header.go
+++ pkg/devfile/header.go
@@ -1,5 +1,10 @@
 package devfile
 
+import (
+	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+
 // DevfileHeader describes the structure of the devfile-specific top-level fields
 // that are not part of the K8S API structures
 type DevfileHeader struct {
@@ -13,6 +18,10 @@ type DevfileHeader struct {
 	Metadata DevfileMetadata `json:"metadata,omitempty"`
 }
 
+// JSONObject provides a way to add a map of arbitrary YAML/JSON
+// objects.
+type JSONObject map[string]apiext.JSON
+
 type DevfileMetadata struct {
 	// Optional devfile name
 	// +optional
@@ -22,4 +31,6 @@ type DevfileMetadata struct {
 	// +optional
 	// +kubebuilder:validation:Pattern=^([0-9]+)\.([0-9]+)\.([0-9]+)(\-[0-9a-z-]+(\.[0-9a-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$
 	Version string `json:"version,omitempty"`
+
+	JSONObject `json:",inline"`
 }
