3a1c4bfc65319a8dff20a4718d311d6a4edad454
Angel Misevski <amisevsk@redhat.com>
2021-01-25 16:00:45 -0500

    Add support for external volumes in Volume components
    Support the use case of mounting an existing secret, configmap, or
    persistent volume into a devfile component by adding a field 'external'
    to volume components. When not empty, the volume component is taken as
    referring to some existing resource that should be mounted to components
    as required.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/apis/workspaces/v1alpha1/volumeComponent.go
+++ pkg/apis/workspaces/v1alpha1/volumeComponent.go
@@ -15,4 +15,38 @@ type Volume struct {
 	// +optional
 	// Size of the volume
 	Size string `json:"size,omitempty"`
+
+	// External defines information about volumes that exist outside of the current workspace.
+	// They are not created or deleted while processing a devfile but are still mounted into
+	// component containers. When left empty, it is assumed that a new volume is to be created.
+	//
+	// Note: External volumes should be used with care, as they make devfiles less portable. It
+	// is assumed that external volumes exist already.
+	// +optional
+	External ExistingVolumeRef `json:"external,omitempty"`
 }
+
+// ExistingVolumeRef is a refernce to a volume that exists outside the lifecycle of components
+type ExistingVolumeRef struct {
+	// Name defines the name of the resource
+	Name string `json:"name"`
+	// Type defines the type of the resource:
+	//
+	// - `storage` specifies that this volume refers to a PersistentVolumeClaim
+	// - `configmap` specifies that this volume refers to a ConfigMap
+	// - `secret` specifies that this volume refers to a Secret
+	// kubebuilder:validation:Enum="persistent,configmap,secret"
+	Type ExistingVolumeType `json:"type"`
+}
+
+// ExistingVolumeType defines the type of an external Volume
+type ExistingVolumeType string
+
+const (
+	// PersistentVolumeType specifies persistent storage, e.g. a PersistentVolumeClaim
+	PersistentVolumeType ExistingVolumeType = "persistent"
+	// ConfigmapVolumeType specifies a configmap
+	ConfigmapVolumeType ExistingVolumeType = "configmap"
+	// SecretVolumeType specifies a secret
+	SecretVolumeType ExistingVolumeType = "secret"
+)
