538cfad73101a086854b4110c44edf1cf6bb056b
Angel Misevski <amisevsk@redhat.com>
2021-04-26 11:36:43 -0400

    Remove GitHub-type projects from v1alpha2 spec
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- pkg/apis/workspaces/v1alpha2/projects.go
+++ pkg/apis/workspaces/v1alpha2/projects.go
@@ -51,12 +51,11 @@ type StarterProject struct {
 // Only one of the following project sources may be specified.
 // If none of the following policies is specified, the default one
 // is AllowConcurrent.
-// +kubebuilder:validation:Enum=Git;Github;Zip;Custom
+// +kubebuilder:validation:Enum=Git;Zip;Custom
 type ProjectSourceType string
 
 const (
 	GitProjectSourceType    ProjectSourceType = "Git"
-	GitHubProjectSourceType ProjectSourceType = "Github"
 	ZipProjectSourceType    ProjectSourceType = "Zip"
 	CustomProjectSourceType ProjectSourceType = "Custom"
 )
@@ -72,10 +71,6 @@ type ProjectSource struct {
 	// Project's Git source
 	// +optional
 	Git *GitProjectSource `json:"git,omitempty"`
-
-	// Project's GitHub source. Deprecated, use `Git` instead
-	// +optional
-	Github *GithubProjectSource `json:"github,omitempty"`
 
 	// Project's Zip source
 	// +optional
@@ -127,9 +122,5 @@ type CheckoutFrom struct {
 }
 
 type GitProjectSource struct {
-	GitLikeProjectSource `json:",inline"`
-}
-
-type GithubProjectSource struct {
 	GitLikeProjectSource `json:",inline"`
 }
