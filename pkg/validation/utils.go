7dc039839a8b8d5ccf583c2beb599e1ecc3cd9a7
Stephanie <yangcao@redhat.com>
2021-05-03 12:05:47 -0400

    update the attribute keys to use prefix: api.devfile.io
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- pkg/validation/utils.go
+++ pkg/validation/utils.go
@@ -11,11 +11,11 @@ import (
 // the value of those keys is the resource information
 const (
 	// attribute key of the imported element resource information
-	ImportSourceAttribute = "library.devfile.io/imported-from"
+	ImportSourceAttribute = "api.devfile.io/imported-from"
 	// attribute key of the parent overridden element resource information
-	ParentOverrideAttribute = "library.devfile.io/parent-override-from"
+	ParentOverrideAttribute = "api.devfile.io/parent-override-from"
 	// attribute key of the plugin overridden element resource information
-	PluginOverrideAttribute = "library.devfile.io/plugin-override-from"
+	PluginOverrideAttribute = "api.devfile.io/plugin-override-from"
 )
 
 // getCommandsMap iterates through the commands and returns a map of command
