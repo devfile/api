f9338bab470f9f88377e76228b666c9b13dc9d2b
Kim Tsao <84398375+kim-tsao@users.noreply.github.com>
2022-06-10 17:52:09 -0400

    licence updates (#869)
    Signed-off-by: Kim Tsao <ktsao@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
-# Copyright (c) 2020 Red Hat, Inc.
-# This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0/
+#   Copyright 2020-2022 Red Hat, Inc.
 #
-# SPDX-License-Identifier: EPL-2.0
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-# Contributors:
-#   Red Hat, Inc. - initial API and implementation
-
-# Based on https://github.com/che-incubator/chectl/blob/master/make-release.sh
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 set -e
 set -u
