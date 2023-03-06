04373ec72c7d9148902bb1c5489bc523ffcbf8bd
Michael Valdron <mvaldron@redhat.com>
2023-03-06 15:33:03 -0500

    fix gosec v2.14.0 (#1034)
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- run_gosec.sh
+++ run_gosec.sh
@@ -3,7 +3,7 @@
 
 if ! command -v gosec 2> /dev/null
 then
-  echo "error gosec must be installed with this command: go install github.com/securego/gosec/v2/cmd/gosec@latest" && exit 1
+  echo "error gosec must be installed with this command: go install github.com/securego/gosec/v2/cmd/gosec@v2.14.0" && exit 1
 fi
 
 gosec -no-fail -fmt=sarif -out=gosec.sarif -exclude-dir test  -exclude-dir generator  ./...
